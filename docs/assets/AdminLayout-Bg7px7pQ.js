74eae6dd6a2c69fa5c70632021d32639561eca96
Carles Labrana <carles.labrana@idealiveconsulting.com>
2025-09-15 13:23:28 +0200

    DOCS para publicacion en GITHub pages

---
+++ docs/assets/AdminLayout-Bg7px7pQ.js
@@ -0,0 +1,1 @@
+import{j as s,O as t}from"./index-DRKg8DrR.js";const a=()=>s.jsxs("div",{className:"bg-blue-400",children:[s.jsx("h1",{children:"Admin Layout"}),s.jsx(t,{})]});export{a as AdminLayout,a as default};
